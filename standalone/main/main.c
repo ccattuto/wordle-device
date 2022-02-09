5cf712d7999107d69a9a40d34e6f03fc0f74d768
Ciro Cattuto <ciro.cattuto@gmail.com>
2022-02-09 23:56:40 +0100

    typos / minor

--- standalone/main/main.c
+++ standalone/main/main.c
@@ -2,15 +2,13 @@
     Wordle Device for the ESP32C3 RBG development board
 
     Written in 2022 by Ciro Cattuto <ciro.cattuto@gmail.com>
-    
+
     To the extent possible under law, the author(s) have dedicated all copyright
     and related and neighboring rights to this software to the public domain worldwide.
     This software is distributed without any warranty.
     You should have received a copy of the CC0 Public Domain Dedication along with this software.
     If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 */
-
-#include "freertos/FreeRTOS.h"
 
 #include "esp_system.h"
 #include "esp_log.h"
